1834f65f9b36ab412a51188cfb7f8ab0570f425b
phertyameen <aminubabafatima8@gmail.com>
2025-06-01 10:07:52 +0100

    correct the implementation to apply the restriction to the login not register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,25 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register-user.dto';
-import { ApiTags, ApiOperation, ApiResponse, ApiBadRequestResponse } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
 import { User } from './entities/user.entity';
-import { Auth } from './decorators/auth.decorator';
-import { authTypes } from './enums/authTypes.enum';
 import { RoleDecorator } from './decorators/role.decorator';
 import { UserRole } from './enums/userRole.enum';
 import { RolesGuard } from './guards/role.guard';
+import { LogInDto } from './dto/loginDto';
+import { AuthGuardGuard } from './guards/auth.guard';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -15,17 +27,26 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  @RoleDecorator(UserRole.ADMIN)
-  // Import and add the jwt auth guard
-  @UseGuards(/* JwtAuthGuard, */ RolesGuard)
   @ApiOperation({ summary: 'Register a new user as Freelancer or Recruiter' })
   @ApiResponse({
     status: 201,
     description: 'User successfully registered',
-    type: User, 
+    type: User,
   })
-  @ApiBadRequestResponse({ description: 'Validation failed or email already exists' })
+  @ApiBadRequestResponse({
+    description: 'Validation failed or email already exists',
+  })
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
+  }
+
+  @Post('/signIn')
+  @RoleDecorator(UserRole.ADMIN)
+  @UseGuards(AuthGuardGuard, RolesGuard)
+  @HttpCode(HttpStatus.OK)
+
+  /**signin class */
+  public async SignIn(@Body() signInDto: LogInDto) {
+    return await this.authService.SignIn(signInDto);
   }
 }
