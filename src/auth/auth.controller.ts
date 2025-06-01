2457e7262ff78e3d822308c002aff0a096d161ed
phertyameen <aminubabafatima8@gmail.com>
2025-06-01 09:01:31 +0100

    feat(auth): restrict admin login endpoint to admin users only

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register-user.dto';
 import { ApiTags, ApiOperation, ApiResponse, ApiBadRequestResponse } from '@nestjs/swagger';
 import { User } from './entities/user.entity';
+import { Auth } from './decorators/auth.decorator';
+import { authTypes } from './enums/authTypes.enum';
+import { RoleDecorator } from './decorators/role.decorator';
+import { UserRole } from './enums/userRole.enum';
+import { RolesGuard } from './guards/role.guard';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -10,6 +15,9 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @RoleDecorator(UserRole.ADMIN)
+  // Import and add the jwt auth guard
+  @UseGuards(/* JwtAuthGuard, */ RolesGuard)
   @ApiOperation({ summary: 'Register a new user as Freelancer or Recruiter' })
   @ApiResponse({
     status: 201,
